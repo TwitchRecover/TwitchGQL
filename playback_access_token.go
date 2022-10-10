bfd792741e43161383e923b03666c825431a4709
Daylam Tayari <git@tayari.gg>
2022-10-09 21:45:02 -0700

    Created and completed playback access token type

--- playback_access_token.go
+++ playback_access_token.go
@@ -1,5 +1,11 @@
 package twitchgql
 
+import (
+	"strconv"
+
+	"github.com/json-iterator/go"
+)
+
 type PlaybackAccessToken struct {
 	request  PlaybackAccessTokenRequest
 	response PlaybackAccessTokenResponse
@@ -22,4 +28,33 @@ type PlaybackAccessTokenRequestParams struct {
 type PlaybackAccessTokenResponse struct {
 	signature string
 	value     string
+}
+
+func (pat *PlaybackAccessToken) RequestParser() (string, error) {
+	if pat.request == (PlaybackAccessTokenRequest{}) {
+		return "", nil
+	}
+	query := `playbackAccessToken(params:{`
+	query += `platform:"` + pat.request.params.platform + `",`
+	query += `playerType:"` + pat.request.params.playerType + `",`
+	query += `playerBackend:"` + pat.request.params.playerBackend + `",`
+	query += `hasAdblock:` + strconv.FormatBool(pat.request.params.hasAdblock) + `,`
+	query += `disableHTTPs:` + strconv.FormatBool(pat.request.params.disableHTTPs) + `,`
+	query += `}){`
+	if pat.request.signature {
+		query += `signature,`
+	}
+	if pat.request.value {
+		query += `value,`
+	}
+	return query + `},`, nil
+}
+
+func (pat *PlaybackAccessToken) ResponseParser(res []byte) {
+	if pat.request.signature {
+		pat.response.signature = jsoniter.Get(res, "signature").ToString()
+	}
+	if pat.request.value {
+		pat.response.value = jsoniter.Get(res, "value").ToString()
+	}
 }
