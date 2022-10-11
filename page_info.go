21a1fc73101988a3443880f36711e1f5c73500e3
Daylam Tayari <git@tayari.gg>
2022-10-11 02:20:59 -0700

    Created page info type

---
+++ page_info.go
@@ -0,0 +1,38 @@
+package twitchgql
+
+import jsoniter "github.com/json-iterator/go"
+
+type PageInfo struct {
+	Request  PageInfoRequest
+	Response PageInfoResponse
+}
+
+type PageInfoRequest struct {
+	hasNextPage     bool
+	hasPreviousPage bool
+}
+
+type PageInfoResponse struct {
+	hasNextPage     bool
+	hasPreviousPage bool
+}
+
+func (pi *PageInfo) RequestParser() (string, error) {
+	if pi.Request == (PageInfoRequest{}) {
+		return "", nil
+	}
+	query := `pageInfo{`
+	if pi.Request.hasNextPage {
+		query += `hasNextPage,`
+	}
+	if pi.Request.hasPreviousPage {
+		query += `hasPreviousPage,`
+	}
+	return query + `},`, nil
+}
+
+func (pi *PageInfo) ResponseParser(res []byte) {
+	res, _ = jsoniter.Marshal(jsoniter.Get(res, "pageInfo"))
+	pi.Response.hasNextPage = jsoniter.Get(res, "hasNextPage").ToBool()
+	pi.Response.hasPreviousPage = jsoniter.Get(res, "hasPreviousPage").ToBool()
+}
